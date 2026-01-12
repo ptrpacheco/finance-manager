3e0f3c4dfdb701b3701b9811f878690a11bf3a74
Pietro Pacheco <pietro.pacheco19@gmail.com>
2026-01-12 00:24:28 -0300

    Setup fonts

---
+++ src/app/_layout.tsx
@@ -0,0 +1,50 @@
+import { SplashScreen, Stack } from 'expo-router';
+import '@/styles/global.css';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import SafeScreen from '@/components/SafeScreen';
+import { StatusBar } from 'expo-status-bar';
+import { useFonts } from 'expo-font';
+import { useEffect } from 'react';
+
+export default function RootLayout() {
+  const [loaded] = useFonts({
+    'Poppins-Light': require('@/assets/fonts/Poppins-Light.ttf'),
+    'Poppins-Regular': require('@/assets/fonts/Poppins-Regular.ttf'),
+    'Poppins-Italic': require('@/assets/fonts/Poppins-Italic.ttf'),
+    'Poppins-SemiBold': require('@/assets/fonts/Poppins-SemiBold.ttf'),
+    'Poppins-Bold': require('@/assets/fonts/Poppins-Bold.ttf'),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hide();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
+  return (
+    <SafeAreaProvider>
+      <SafeScreen>
+        <Stack>
+          <Stack.Screen name="index" options={{ headerShown: false }} />
+          <Stack.Screen
+            name="(auth)"
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen
+            name="(app)"
+            options={{
+              headerShown: false,
+            }}
+          />
+        </Stack>
+      </SafeScreen>
+      <StatusBar style='light' />
+    </SafeAreaProvider>
+  );
+}
